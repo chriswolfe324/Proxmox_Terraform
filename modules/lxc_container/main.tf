98a424ffcdc3ee1dad1f3a6cc758b20fe77810f2
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-02-24 08:09:52 -0500

    Added reusable container module

---
+++ modules/lxc_container/main.tf
@@ -0,0 +1,64 @@
+resource "proxmox_virtual_environment_container" "container" {
+  node_name    = var.node_name
+  vm_id        = var.vm_id
+  unprivileged = true
+  started      = true
+  protection   = false
+
+  console {
+    enabled   = true
+    tty_count = 2
+    type      = "tty"
+  }
+
+  disk {
+    datastore_id = var.disk_datastore_id
+    size         = var.disk_size
+  }
+
+  initialization {
+    hostname = var.hostname
+
+    ip_config {
+      ipv4 {
+        address = var.ipv4_address
+        gateway = var.gateway
+      }
+    }
+
+    dynamic "dns" {
+      for_each = var.enable_dns ? [1] : []
+      content {
+        domain  = var.dns_domain
+        servers = var.dns_servers
+      }
+    }
+  }
+
+  memory {
+    dedicated = var.memory_dedicated
+    swap      = var.memory_swap
+  }
+
+  network_interface {
+    name        = var.network_interface_name
+    bridge      = var.network_interface_bridge
+    mac_address = var.mac_address
+    firewall    = true
+  }
+
+  startup {
+    order = var.startup_order
+  }
+
+  lifecycle {
+    ignore_changes = [
+      operating_system,
+      start_on_boot,
+      timeout_clone,
+      timeout_create,
+      timeout_delete,
+      timeout_update,
+    ]
+  }
+}
